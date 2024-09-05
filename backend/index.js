181e36e54a49a0634943bbcaa4babbb01078fba7
Alvin Ben <69302420+alvinbengeorge@users.noreply.github.com>
2024-09-05 08:24:08 +0000

    feat: basic controller and routes

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,17 @@
-import Express from "express";
-const app = Express();
+import express from "express";
+import basic from "./routes/basic.js";
 
-app.use(Express.json());
+const app = express();
+
+app.use(express.json());
+app.use("/basic", basic);
 
 app.get("/", (req, res) => {
     res.send({
-        "message": "Hello World"
+        message: "Hello World",
     });
 });
 
 app.listen(8000, () => {
-    console.log("Listening in 8000")
-})
+    console.log("Listening in 8000");
+});
