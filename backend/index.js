bb11f5b403d48adc05e4012b915d30db1e37fd53
Alvin Ben <69302420+alvinbengeorge@users.noreply.github.com>
2024-09-06 14:09:30 +0000

    feat: notes backend with array

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,12 @@
 import express from "express";
 import basic from "./routes/basic.js";
+import notes from "./routes/notes.js";
 
 const app = express();
 
 app.use(express.json());
 app.use("/basic", basic);
+app.use("/notes", notes);
 
 app.get("/", (req, res) => {
     res.send({
