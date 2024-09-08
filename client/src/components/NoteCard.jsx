5c461598f9cd997a161378279b304f9faaf33868
SaanviKumar13 <saanviprashantkumar@gmail.com>
2024-09-08 12:31:36 +0530

    feat: css for note card

--- client/src/components/NoteCard.jsx
+++ client/src/components/NoteCard.jsx
@@ -1,15 +1,14 @@
 import Button from "../components/Button";
+import "../styles/global.css";
 
 const NoteCard = ({ note, onDelete, onEdit }) => {
   return (
-    <div className="relative bg-gray-50 shadow-lg rounded-xl p-6 flex flex-col justify-between hover:shadow-2xl border-2 border-gray-900">
+    <div className="note-card">
       <div>
-        <h2 className="text-2xl font-bold text-gray-800 mb-4 tracking-wide">
-          {note.title}
-        </h2>
-        <p className="text-gray-700 text-lg">{note.content}</p>
+        <h2>{note.title}</h2>
+        <p>{note.content}</p>
       </div>
-      <div className="flex justify-end space-x-4 mt-6">
+      <div className="note-card-buttons">
         <Button text="Edit" onClick={() => onEdit(note)} type="edit" />
         <Button text="Delete" onClick={() => onDelete(note.id)} type="delete" />
       </div>
