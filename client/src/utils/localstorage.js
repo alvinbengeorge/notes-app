7e866a306fd8391be84fbac7872f4adac6d60471
SaanviKumar13 <saanviprashantkumar@gmail.com>
2024-09-08 23:40:00 +0530

    chore: cleanup

--- client/src/utils/localstorage.js
+++
@@ -1,8 +0,0 @@
-export const getNotes = () => {
-  const notes = localStorage.getItem('notes');
-  return notes ? JSON.parse(notes) : [];
-};
-
-export const saveNotes = (notes) => {
-  localStorage.setItem('notes', JSON.stringify(notes));
-};
